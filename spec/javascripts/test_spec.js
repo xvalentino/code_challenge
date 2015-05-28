aee533de06d94c9cd3044a9513847888d5a2afea
Valentino Espinoza <xvalentino13@gmail.com>
2015-05-28 11:11:36 -0600

    teaspoon is all setup and running

--- spec/javascripts/test_spec.js
+++ spec/javascripts/test_spec.js
@@ -2,8 +2,4 @@ describe("Testing", function() {
   it("is going so smoothly", function() {
     expect(true).to.equal(true);
   });
-
-  it("is not going so smoothly", function() {
-    expect(true).to.equal(false);
-  });
 });
