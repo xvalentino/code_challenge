e341325e5fb21140d9459f0edfa54049086f8b3e
Valentino Espinoza <xvalentino13@gmail.com>
2015-05-28 10:36:18 -0600

    emails are being sent to notify admins

--- app/mailers/admin_order_mailer.rb
+++ app/mailers/admin_order_mailer.rb
@@ -2,8 +2,8 @@ class AdminOrderMailer < ActionMailer::Base
   default from: "codechallengesrule@example.com"
 
   def email_admin_about_order(user, order)
-    mail(to: 'storedom-overlord@example.com', 
-         subject: 'An order was just placed!',
-         body: "#{user.name} has just placed Order #{order.id}")
+    @user = user
+    @order = order
+    mail(to: 'email@email.com', subject: 'An order was just placed!')
   end
 end
